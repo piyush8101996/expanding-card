69d1259ea908b09097819f329db2ec039546fd84
singh piyush <singhpiyush6101996@gmail.com>
2022-04-08 05:39:33 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+let num=[15,25,35,40];
+let fliternum=num.filter(num=>num+15)
+console.log(fliternum)
